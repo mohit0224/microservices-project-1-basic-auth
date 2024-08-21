93c55987b69a720c84c48a66c1a95f31f09cd248
mohit0224 <mohitdheer24+git@gmail.com>
2024-08-21 13:12:46 +0530

    test

--- microservices/auth.microservices.js
+++ microservices/auth.microservices.js
@@ -6,7 +6,7 @@ import authRoutes from "../routes/auth.route.js";
 import dbConnect from "../config/dbConnect.config.js";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10001;
 const corsOption = {
 	origin: "*",
 	credentials: true,
